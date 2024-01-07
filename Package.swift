8f035cdb36864ad629ec5fa07074f7fe13eb13a3
Arman Galstyan <armangalstyan9597@gmail.com>
2024-01-07 22:01:51 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-        .package(path: "../Driven/Driven/Driven/")
+        .package(url: "https://github.com/Arman1997/Driven", branch: "master"),
+        .package(url: "https://github.com/Arman1997/DeclarativeSwiftSyntax", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -19,7 +20,8 @@ let package = Package(
             name: "DrivenCLI",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Driven", package: "Driven")
+                .product(name: "Driven", package: "Driven"),
+                .product(name: "DeclarativeSwiftSyntax", package: "DeclarativeSwiftSyntax")
             ]
         ),
     ]
